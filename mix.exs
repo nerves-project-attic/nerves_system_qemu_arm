18d8bde044ebb1a806526830a4341604c5f0683d
Justin Schneck <jschneck@mac.com>
2016-04-02 22:08:10 -0400

    updated license and package info for hex

--- mix.exs
+++ mix.exs
@@ -6,6 +6,8 @@ defmodule NervesSystemQemuArm.Mixfile do
     version: "0.4.0-rc3",
     elixir: "~> 1.2",
     compilers: Mix.compilers ++ [:nerves_system],
+    description: description,
+    package: package,
     deps: deps]
   end
 
@@ -14,10 +16,20 @@ defmodule NervesSystemQemuArm.Mixfile do
   end
 
   defp deps do
-   [
-     {:nerves_system, path: "../nerves_system"},
+    [{:nerves_system, path: "../nerves_system"},
      {:nerves_system_br, path: "../nerves-system-br"},
-     {:nerves_toolchain_arm_unknown_linux_gnueabihf, path: "../nerves_toolchain_arm_unknown_linux_gnueabihf"}
-   ]
+     {:nerves_toolchain_arm_unknown_linux_gnueabihf, path: "../nerves_toolchain_arm_unknown_linux_gnueabihf"}]
+  end
+
+  defp description do
+    """
+    Nerves System - QEMU Arm
+    """
+  end
+
+  defp package do
+    [maintainers: ["Frank Hunleth", "Justin Schneck"],
+     licenses: ["Apache 2.0"],
+     links: %{"Github" => "https://github.com/nerves-project/nerves_system_qemu_arm"}]
   end
 end
