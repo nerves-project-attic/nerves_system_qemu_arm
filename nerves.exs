bc1aa984db7c86b130c39784349278d7bd3ba5ab
Justin Schneck <jschneck@mac.com>
2016-04-04 16:38:42 -0400

    point build_platform to module

--- nerves.exs
+++ nerves.exs
@@ -1,8 +1,8 @@
 use Mix.Config
 
 config :nerves_system_qemu_arm, :nerves_env,
-  type:  :system,
-  build_platform: :nerves_system_br,
+  type: :system,
+  build_platform: Nerves.System.Platforms.BR,
   bakeware: [target: "qemu_arm", recipe: "nerves/qemu_arm"],
   ext: [
     defconfig: "nerves_defconfig"
