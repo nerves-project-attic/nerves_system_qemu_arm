7e46cfd31908f0b94f9d0a0d30542e0f88a2a5ce
Justin Schneck <jschneck@mac.com>
2016-05-09 10:27:51 -0400

    deploy using travis

--- nerves.exs
+++ nerves.exs
@@ -8,7 +8,7 @@ version =
 config :nerves_system_qemu_arm, :nerves_env,
   type: :system,
   mirrors: [
-    "https://github.com/nerves-project/nerves_system_br/releases/download/v#{version}/nerves_system_qemu_arm-nerves-v#{version}.tar.gz"],
+    "https://github.com/nerves-project/nerves_system_qemu_arm/releases/download/v#{version}/nerves_system_qemu_arm-v#{version}.tar.gz"],
   build_platform: Nerves.System.Platforms.BR,
   build_config: [
     defconfig: "nerves_defconfig"
