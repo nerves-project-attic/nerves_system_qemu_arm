486fc57855c45e32c7cc06939ec6cd339c712e05
Frank Hunleth <fhunleth@troodon-software.com>
2016-06-20 22:25:41 -0400

    Move erlinit.config here; update refs

--- nerves.exs
+++ nerves.exs
@@ -12,5 +12,8 @@ config :nerves_system_qemu_arm, :nerves_env,
     "https://github.com/nerves-project/nerves_system_qemu_arm/releases/download/v#{version}/nerves_system_qemu_arm-v#{version}.tar.gz"],
   build_platform: Nerves.System.Platforms.BR,
   build_config: [
-    defconfig: "nerves_defconfig"
+    defconfig: "nerves_defconfig",
+    package_files: [
+      "rootfs-additions"
+    ]
   ]
