af1f046a282ac73a99441a76d1f16382f696140b
Justin Schneck <jschneck@mac.com>
2016-06-29 12:10:53 -0400

    support for fetching from pre

--- nerves.exs
+++ nerves.exs
@@ -9,7 +9,8 @@ config :nerves_system_qemu_arm, :nerves_env,
   type: :system,
   version: version,
   mirrors: [
-    "https://github.com/nerves-project/nerves_system_qemu_arm/releases/download/v#{version}/nerves_system_qemu_arm-v#{version}.tar.gz"],
+    "https://github.com/nerves-project/nerves_system_qemu_arm/releases/download/v#{version}/nerves_system_qemu_arm-v#{version}.tar.gz",
+    "https://s3.amazonaws.com/nerves/artifacts/nerves_system_qemu_arm-#{version}.tar.gz"],
   build_platform: Nerves.System.Platforms.BR,
   build_config: [
     defconfig: "nerves_defconfig",
