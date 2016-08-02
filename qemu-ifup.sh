0c048ef8757b731520e9c9efbec14e348a5bfe2a
Mike Gogulski <mike@gogulski.com>
2016-08-02 23:54:54 +0200

    add linux networking instructions and scripting

--- qemu-ifup.sh
+++ qemu-ifup.sh
@@ -3,4 +3,12 @@ case $OSTYPE in
   darwin*)
     ifconfig bridge1 addm "$1"
     ;;
+  linux-gnu*)
+    # create the tap interface
+    sudo ip tuntap add dev tap0 mode tap user `whoami`
+    # enable the tap interface
+    sudo ip link set tap0 up
+    # add tap interface to existing bridge
+    sudo brctl addif bridge1 tap0
+    ;;
 esac
