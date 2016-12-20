daddabd2bbc008a58300c30b4b381f172d70ba74
Justin Schneck <jschneck@mac.com>
2016-12-19 21:17:56 -0500

    depend on bootstrap and start the env from the top of the stack

--- mix.exs
+++ mix.exs
@@ -9,6 +9,8 @@ defmodule NervesSystemQemuArm.Mixfile do
     [app: :nerves_system_qemu_arm,
     version: @version,
     elixir: "~> 1.2",
+    archives: [nerves_bootstrap: "~> 0.2.1"],
+    aliases: ["deps.precompile": ["nerves.env", "deps.precompile"]],
     compilers: Mix.compilers ++ [:nerves_package],
     description: description(),
     package: package(),
