07abc7b2d2efe579686688523311d0580846e857
Frank Hunleth <fhunleth@troodon-software.com>
2017-03-25 16:35:07 -0400

    Bump deps

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule NervesSystemQemuArm.Mixfile do
   def project do
     [app: :nerves_system_qemu_arm,
     version: @version,
-    elixir: "~> 1.2",
+     elixir: "~> 1.3",
     compilers: Mix.compilers ++ [:nerves_package],
     description: description(),
     package: package(),
@@ -21,9 +21,9 @@ defmodule NervesSystemQemuArm.Mixfile do
   end
 
   defp deps do
-    [{:nerves, "~> 0.4"},
-     {:nerves_system_br, "~> 0.9.2"},
-     {:nerves_toolchain_arm_unknown_linux_gnueabihf, "~> 0.10.0"}]
+    [{:nerves, "~> 0.5", runtime: false },
+     {:nerves_system_br, "~> 0.9.2", runtime: false },
+     {:nerves_toolchain_arm_unknown_linux_gnueabihf, "~> 0.10.0", runtime: false}]
   end
 
   defp description do
