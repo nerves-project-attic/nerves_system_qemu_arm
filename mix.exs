7cbf774a9dca2b1aa95ed9b8e7a5ab6788762342
Justin Schneck <jschneck@mac.com>
2017-11-19 22:54:06 -0500

    update to nerves 0.8 and nerves_system_br 0.15.0

--- mix.exs
+++ mix.exs
@@ -1,30 +1,50 @@
 defmodule NervesSystemQemuArm.Mixfile do
   use Mix.Project
 
+  @app :nerves_system_qemu_arm
   @version Path.join(__DIR__, "VERSION")
     |> File.read!
     |> String.trim
 
   def project do
-    [app: :nerves_system_qemu_arm,
-     version: @version,
-     elixir: "~> 1.4",
-     compilers: Mix.compilers ++ [:nerves_package],
-     description: description(),
-     package: package(),
-     deps: deps(),
-     aliases: ["deps.precompile": ["nerves.env", "deps.precompile"]]]
+    [
+      app: @app,
+      version: @version,
+      elixir: "~> 1.4",
+      compilers: Mix.compilers ++ [:nerves_package],
+      nerves_package: nerves_package(),
+      description: description(),
+      package: package(),
+      deps: deps(),
+      aliases: ["deps.precompile": ["nerves.env", "deps.precompile"]]
+    ]
   end
 
   def application do
-   []
+    []
+  end
+
+  def nerves_package do
+    [
+      type: :system,
+      version: @version,
+      compiler: :nerves_package,
+      artifact_url: [
+        "https://github.com/nerves-project/#{@app}/releases/download/v#{@version}/#{@app}-v#{@version}.tar.gz",
+      ],
+      platform: Nerves.System.BR,
+      platform_config: [
+        defconfig: "nerves_defconfig",
+      ],
+      checksum: package_files()
+    ]
   end
 
   defp deps do
     [
-      {:nerves, "~> 0.7", runtime: false },
-      {:nerves_system_br, "~> 0.14.1", runtime: false },
-      {:nerves_toolchain_arm_unknown_linux_gnueabihf, "~> 0.11.0", runtime: false}
+      {:nerves, "~> 0.8", runtime: false},
+      {:nerves_system_br, "~> 0.15.0", runtime: false, app: false},
+      {:nerves_toolchain_arm_unknown_linux_gnueabihf, "~> 0.12.0", runtime: false, app: false}
     ]
   end
 
@@ -35,11 +55,24 @@ defmodule NervesSystemQemuArm.Mixfile do
   end
 
   defp package do
-    [maintainers: ["Frank Hunleth", "Justin Schneck"],
-     files: ["LICENSE", "mix.exs", "nerves_defconfig", "nerves.exs",
-             "README.md", "VERSION", "rootfs_overlay", "lib",
-             "priv"],
-     licenses: ["Apache 2.0"],
-     links: %{"Github" => "https://github.com/nerves-project/nerves_system_qemu_arm"}]
+    [
+      maintainers: ["Frank Hunleth", "Justin Schneck"],
+      files: package_files(),
+      licenses: ["Apache 2.0"],
+      links: %{"Github" => "https://github.com/nerves-project/#{@app}"}
+    ]
+  end
+
+  defp package_files do
+    [
+      "LICENSE",
+      "mix.exs",
+      "nerves_defconfig",
+      "README.md",
+      "VERSION",
+      "rootfs_overlay",
+      "lib",
+      "priv"
+    ]
   end
 end
