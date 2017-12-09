c7da2dc145d923cc09b2bec9b96d13761299b367
Frank Hunleth <fhunleth@troodon-software.com>
2017-12-08 21:39:22 -0500

    Sync with rpi0
    This makes for easier directory comparisons.

--- mix.exs
+++ mix.exs
@@ -11,7 +11,7 @@ defmodule NervesSystemQemuArm.Mixfile do
       app: @app,
       version: @version,
       elixir: "~> 1.4",
-      compilers: Mix.compilers ++ [:nerves_package],
+      compilers: Mix.compilers() ++ [:nerves_package],
       nerves_package: nerves_package(),
       description: description(),
       package: package(),
@@ -27,14 +27,12 @@ defmodule NervesSystemQemuArm.Mixfile do
   def nerves_package do
     [
       type: :system,
-      version: @version,
-      compiler: :nerves_package,
       artifact_url: [
-        "https://github.com/nerves-project/#{@app}/releases/download/v#{@version}/#{@app}-v#{@version}.tar.gz",
+        "https://github.com/nerves-project/#{@app}/releases/download/v#{@version}/#{@app}-v#{@version}.tar.gz"
       ],
       platform: Nerves.System.BR,
       platform_config: [
-        defconfig: "nerves_defconfig",
+        defconfig: "nerves_defconfig"
       ],
       checksum: package_files()
     ]
@@ -43,8 +41,8 @@ defmodule NervesSystemQemuArm.Mixfile do
   defp deps do
     [
       {:nerves, "~> 0.8", runtime: false},
-      {:nerves_system_br, "~> 0.15.0", runtime: false, app: false},
-      {:nerves_toolchain_arm_unknown_linux_gnueabihf, "~> 0.12.1", runtime: false, app: false}
+      {:nerves_system_br, "~> 0.15.1", runtime: false},
+      {:nerves_toolchain_arm_unknown_linux_gnueabihf, "~> 0.12.1", runtime: false}
     ]
   end
 
@@ -71,6 +69,9 @@ defmodule NervesSystemQemuArm.Mixfile do
       "README.md",
       "VERSION",
       "rootfs_overlay",
+      "fwup.conf",
+      "linux-4.4.defconfig",
+      "post-createfs.sh",
       "lib",
       "priv"
     ]
